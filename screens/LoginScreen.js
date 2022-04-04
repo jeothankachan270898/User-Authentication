69aafe69cb8aece84c4309c1c48fe425a6f5caa4
jeothankachan270898 <jeothankachan270898@gmail.com>
2022-04-04 11:13:19 +0530

    Authentication Finish

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,15 @@
-import { useState } from 'react';
+import { useState ,useContext } from 'react';
 import { Alert } from 'react-native';
 
 import AuthContent from '../components/Auth/AuthContent';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
 import { login } from '../util/auth';
+import { AuthContext } from '../store/auth-context';
 
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const authCtx = useContext(AuthContext);
 
   async function loginHandler({ email, password }) {
     setIsAuthenticating(true);
